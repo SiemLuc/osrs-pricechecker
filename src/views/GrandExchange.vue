3e195f11f1e0b1c7e2141ae22b20a20fb95d117c
Siem Lucassen <siem228@hotmail.com>
2020-03-06 19:50:06 +0100

    Initial commit

---
+++ src/views/GrandExchange.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="GE">
+    <p>GE</p>
+</div>
+
+</template>
+
+<script>
+import {Component, Vue, Prop} from 'vue-property-decorator'
+
+@Component({})
+export default class GrandExchangeView extends Vue{
+
+}
+</script>
+
+<style scoped>
+    .GE{
+        background-color: #443d33;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>osu is gay</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+@Component({})
+export default class HomeView extends Vue{
+
 }
 </script>
